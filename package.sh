2bbaf18b3a7b8581e6061629010718ee64fa536c
psyGamer <psygamer21@gmail.com>
2023-07-28 19:25:00 +0200

    Improved packaging script

--- package.sh
+++ package.sh
@@ -1,1 +1,4 @@
+rm -rd bin obj
+# Compiling in Release mode causes some issues
+dotnet publish -c Debug
 zip -r TASRecorder-v$1.zip everest.yaml bin/TASRecorder.dll bin/TASRecorder.pdb Dialog Graphics
